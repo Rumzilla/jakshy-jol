81f3d8962143872408fe2a7f26a3877ba51cf906
JussVi <osadze_denis_007@mail.ru>
2023-02-14 17:29:26 +0600

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import CountDown from './components/timer';
 import DrivingTestModule from "./modules/driving-test/TestingWindow";
+import HomePage from './pages/HomePage/HomePage';
+
+import './reset/reset.css'
 
 function App() {
   return (
     <div className="App">
       <CountDown />
       <DrivingTestModule />
-      <div>123</div>
-
+      <HomePage />
     </div>
   );
 }
